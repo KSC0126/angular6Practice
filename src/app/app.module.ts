e56bc6ad6a4e36373460df338e7cac7b0a359885
showrya <kollushowrya@gmail.com>
2018-12-21 00:51:22 -0500

    commit master
    there are no branches to this repo . Angular 6 practice and calling online free API to populate the view part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { DetailsComponent } from './details/details.component';
+import { UsersComponent } from './users/users.component';
+import { HttpClientModule} from '@angular/common/http';
+import { DataService } from '../app/data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    DetailsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule 
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
